b33d1fb28d04b4ce077372a45a4e15753c2e62ab
Unknown <parjoona@hotmail.com>
2018-07-10 16:18:39 +0200

    Added ThemeData to make colorscheme changeable

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RandomWordsAreFun',
+      theme: ThemeData(
+        primaryColor: Colors.white
+      ),
       home: RandomWords(),
     );
   }
